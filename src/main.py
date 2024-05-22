9aeebca8ea847c656db593f6fb6a51e068e08454
StephanAkkerman <stephan.akkerman@gmail.com>
2024-05-22 20:00:07 +0200

    Improved looks + increased rainbow length

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
+from datetime import timedelta
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from matplotlib.ticker import FuncFormatter
 from scipy.optimize import curve_fit
 
-# Define colors and labels for the rainbow bands
-colors_labels = {
+# Define constants
+COLORS_LABELS = {
     "#c00200": "Maximum bubble territory",
     "#d64018": "Sell. Seriously, SELL!",
     "#ed7d31": "FOMO Intensifies",
@@ -16,88 +18,113 @@ colors_labels = {
     "#54989f": "BUY!",
     "#4472c4": "Fire sale!",
 }
-# Colors and labels from https://www.coinglass.com/pro/i/bitcoin-rainbow-chart
+BAND_WIDTH = 0.3
+NUM_BANDS = 9
+FIGURE_SIZE = (15, 7)
+BACKGROUND_COLOR = "#0d1117"
+EXTEND_MONTHS = 9
 
 
-# this is your log function
-def logFunc(x, a, b, c):
+def log_func(x, a, b, c):
+    """Logarithmic function for curve fitting."""
     return a * np.log(b + x) + c
 
 
-def get_data():
-    # Load data
-    raw_data = pd.read_csv("bitcoin_data.csv")
+def get_data(file_path):
+    """
+    Load and preprocess data from a CSV file.
 
-    raw_data["Date"] = pd.to_datetime(
-        raw_data["Date"]
-    )  # Ensure that the date is in datetime or graphs might look funny
-    raw_data = raw_data[
-        raw_data["Value"] > 0
-    ]  # Drop all 0 values as they will mess up the regression bands
+    Args:
+        file_path (str): Path to the CSV file.
 
-    # getting your x and y data from the dataframe
+    Returns:
+        pd.DataFrame: Processed data.
+        np.ndarray: Fitted Y data.
+    """
+    raw_data = pd.read_csv(file_path)
+    raw_data["Date"] = pd.to_datetime(raw_data["Date"])
+    raw_data = raw_data[raw_data["Value"] > 0]
     xdata = np.array([x + 1 for x in range(len(raw_data))])
     ydata = np.log(raw_data["Value"])
-
-    # here we ar fitting the curve, you can use 2 data points however I wasn't able to get a graph that looked as good with just 2 points.
-    popt, _ = curve_fit(logFunc, xdata, ydata)
-    # This is our fitted data, remember we will need to get the ex of it to graph it
-    fittedYData = logFunc(xdata, popt[0], popt[1], popt[2])
-
-    return raw_data, fittedYData
+    popt, _ = curve_fit(log_func, xdata, ydata)
+    fitted_ydata = log_func(xdata, popt[0], popt[1], popt[2])
+    return raw_data, fitted_ydata, popt
 
 
-# Create a wider plot with a dark background
-fig, ax = plt.subplots(figsize=(15, 7))
-fig.patch.set_facecolor("#0d1117")  # Set figure face color
-ax.set_facecolor("#0d1117")  # Set axes face color
+def extend_dates(raw_data, months=EXTEND_MONTHS):
+    """
+    Extend the date range of the data by a specified number of months.
 
-# Draw the rainbow bands and create legend handles
-legend_handles = []
+    Args:
+        raw_data (pd.DataFrame): Original data.
+        months (int): Number of months to extend.
 
-raw_data, fittedYData = get_data()
+    Returns:
+        pd.Series: Extended date range.
+    """
+    last_date = raw_data["Date"].max()
+    extended_dates = pd.date_range(
+        start=last_date + timedelta(days=1), periods=months * 30
+    )
+    return pd.concat([raw_data["Date"], pd.Series(extended_dates)])
 
 
 def plot_rainbow(
-    ax, fittedYData, raw_data, num_bands=9, band_width=0.3, legend_handles=[]
+    ax, fitted_ydata, raw_data, popt, num_bands=NUM_BANDS, band_width=BAND_WIDTH
 ):
+    """
+    Plot rainbow bands on the given axis.
+
+    Args:
+        ax (matplotlib.axes._subplots.AxesSubplot): Axis to plot on.
+        fitted_ydata (np.ndarray): Fitted Y data.
+        raw_data (pd.DataFrame): Raw data.
+        num_bands (int): Number of bands.
+        band_width (float): Width of each band.
+    """
+    extended_dates = extend_dates(raw_data)
+    extended_xdata = np.arange(1, len(extended_dates) + 1)
+    extended_fitted_ydata = log_func(extended_xdata, *popt)
+
+    legend_handles = []
     for i in range(num_bands):
-        # Higher values decreases the bands (so the rainbow is lower on the plot)
         i_decrease = 1.5
-        lower_bound = np.exp(fittedYData + (i - i_decrease) * band_width - band_width)
-        upper_bound = np.exp(fittedYData + (i - i_decrease) * band_width)
-        color = list(colors_labels.keys())[::-1][i]
-        label = list(colors_labels.values())[::-1][i]
+        lower_bound = np.exp(
+            extended_fitted_ydata + (i - i_decrease) * band_width - band_width
+        )
+        upper_bound = np.exp(extended_fitted_ydata + (i - i_decrease) * band_width)
+        color = list(COLORS_LABELS.keys())[::-1][i]
+        label = list(COLORS_LABELS.values())[::-1][i]
         ax.fill_between(
-            raw_data["Date"],
-            lower_bound,
-            upper_bound,
-            alpha=1,  # 0.8
-            color=color,
-            label=label,
+            extended_dates, lower_bound, upper_bound, alpha=1, color=color, label=label
         )
         legend_handles.append(
-            plt.Rectangle((0, 0), 1, 1, color=color, alpha=1, label=label)
-        )
-
-
-plot_rainbow(ax, fittedYData, raw_data, legend_handles=legend_handles)
+            plt.Line2D([0], [0], color=color, lw=4, label=label)
+        )  # Changed to Line2D
+    return legend_handles
 
 
 def plot_price(ax, raw_data):
-    # Plot in a with long Y axis and set the color of the Bitcoin price data
-    (btc_line,) = ax.semilogy(
-        raw_data["Date"], raw_data["Value"], color="white", label="BTC price"
-    )  # Change color to white for contrast
+    """
+    Plot Bitcoin price data on the given axis.
 
-    return btc_line
+    Args:
+        ax (matplotlib.axes._subplots.AxesSubplot): Axis to plot on.
+        raw_data (pd.DataFrame): Raw data.
+
+    Returns:
+        matplotlib.lines.Line2D: The line representing the BTC price.
+    """
+    return ax.semilogy(
+        raw_data["Date"].values,
+        raw_data["Value"].values,
+        color="white",
+        label="BTC Price",
+    )[0]
 
 
-btc_line = plot_price(ax, raw_data)
-
-
-# Custom formatter function to display y-axis labels as $0.01, $0.1, $1, $10, $1K, $10K, etc.
 def y_format(y, _):
+    """Custom formatter for Y-axis labels."""
     if y < 1:
         return f"${y:.2f}"
     elif y < 10:
@@ -110,31 +137,87 @@ def y_format(y, _):
         return f"${y/1_000_000:.1f}M".replace(".0M", "M").replace(".", ",")
 
 
-# Apply the custom formatter to the y-axis
-formatter = FuncFormatter(y_format)
-ax.yaxis.set_major_formatter(formatter)
+def configure_plot(ax, raw_data):
+    """
+    Configure the appearance of the plot.
 
-# Set the minimum y-axis value to $0.01
-ax.set_ylim(bottom=0.01)
+    Args:
+        ax (matplotlib.axes._subplots.AxesSubplot): Axis to configure.
+        raw_data (pd.DataFrame): Raw data.
+    """
+    formatter = FuncFormatter(y_format)
+    ax.yaxis.set_major_formatter(formatter)
+    ax.set_ylim(bottom=0.01)
+    ax.set_xlim(
+        [
+            raw_data["Date"].min(),
+            raw_data["Date"].max() + pd.DateOffset(months=EXTEND_MONTHS),
+        ]
+    )
+    ax.tick_params(axis="x", colors="white")
+    ax.tick_params(axis="y", colors="white")
 
-# Set the x-axis limits to fit the data without gaps
-ax.set_xlim([raw_data["Date"].min(), raw_data["Date"].max()])
 
-# Change the color of the tick labels
-ax.tick_params(axis="x", colors="white")
-ax.tick_params(axis="y", colors="white")
+def add_legend(ax):
+    # Create custom legend handles with square markers, including BTC price
+    legend_handles = [
+        plt.Line2D(
+            [0],
+            [0],
+            marker="s",
+            color=BACKGROUND_COLOR,
+            markerfacecolor="white",
+            markersize=10,
+            label="BTC price",
+        )
+    ] + [
+        plt.Line2D(
+            [0],
+            [0],
+            marker="s",
+            color=BACKGROUND_COLOR,
+            markerfacecolor=color,
+            markersize=10,
+            label=label,
+        )
+        for color, label in zip(
+            list(COLORS_LABELS.keys()), list(COLORS_LABELS.values())
+        )
+    ]
 
-# Add the legend above the plot, centered, in a single row
-handles = [btc_line] + legend_handles[::-1]  # Reversed order for legend
-ax.legend(
-    handles=handles,
-    loc="upper center",
-    bbox_to_anchor=(0.5, 1.15),
-    ncol=len(handles),
-    frameon=False,
-    fontsize="small",
-    labelcolor="white",
-)
+    # Add legend
+    ax.legend(
+        handles=legend_handles,
+        loc="upper center",
+        bbox_to_anchor=(0.5, 1.15),
+        ncol=len(legend_handles),
+        frameon=False,
+        fontsize="small",
+        labelcolor="white",
+    )
 
-# Show the plot in wide format
-plt.show()
+
+def main():
+    # Load data
+    raw_data, fitted_ydata, popt = get_data("data/bitcoin_data.csv")
+
+    # Create plot
+    fig, ax = plt.subplots(figsize=FIGURE_SIZE)
+    fig.patch.set_facecolor(BACKGROUND_COLOR)
+    ax.set_facecolor(BACKGROUND_COLOR)
+
+    # Plot rainbow bands and price data
+    plot_rainbow(ax, fitted_ydata, raw_data, popt)
+    plot_price(ax, raw_data)
+
+    # Configure plot appearance
+    configure_plot(ax, raw_data)
+
+    add_legend(ax)
+
+    # Show plot
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()

4f953712d67a297fabc75e85563fba091016ca06
StephanAkkerman <stephan.akkerman@gmail.com>
2024-05-24 22:06:35 +0200

    Updated img

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from plot import create_plot
 from data import get_data
 
 
-def main(save: bool = False, file_path: str = "bitcoin_rainbow_chart.png"):
+def main(save: bool = False, file_path: str = "img/bitcoin_rainbow_chart.png"):
     # Load data
     raw_data, popt = get_data("data/bitcoin_data.csv")
 
